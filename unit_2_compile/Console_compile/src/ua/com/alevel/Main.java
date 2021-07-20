e40975e11c82d2ac7ee911ca87e2af7eb7b758a6
Tetyana <gromova1tanya@gmail.com>
2021-07-20 18:36:12 +0300

    start project

---
+++ unit_2_compile/Console_compile/src/ua/com/alevel/Main.java
@@ -0,0 +1,11 @@
+package ua.com.alevel;
+
+import ua.com.alevel.students.StudentsInfo;
+import ua.com.alevel.education.Education;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Console");
+        study();
+    }
+}
---
+++ unit_2_compile/Console_compile/src/ua/com/alevel/education/Education.java
@@ -0,0 +1,9 @@
+package ua.com.alevel.education;
+
+public class Education {
+
+    public static void study() {
+        System.out.println("Student is learning Console");
+    }
+
+}
