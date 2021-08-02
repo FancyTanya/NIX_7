3d3b9aa93e9da66b7cedb5b60dd9ff9a6801f8c0
Tetyana <gromova1tanya@gmail.com>
2021-08-02 19:04:43 +0300

    add string lib

--- unit_5_strings/src/main/java/Main.java
+++ unit_5_strings/src/main/java/Main.java
@@ -1,2 +1,24 @@
+import libs.reverseString.jar;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("**************************");
+        System.out.println("If you want to reverse the string. Enter number 1");
+        System.out.println("If you want to enter a string and reverse some word. Enter number 2");
+        System.out.println("If you want to enter a string and reverse part of the string using the start and end indices. Enter number 3");
+        System.out.println("**************************");
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("Please, enter the string");
+        Scanner input = new Scanner(System.in);
+        String inputStr = input.nextLine();
+
+        if (number == 1) {
+            System.out.println(inputStr.reverse);
+        }
+
+    }
 }
