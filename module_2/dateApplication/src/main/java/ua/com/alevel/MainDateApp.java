05b8b31cd1f34e3b0abc7b8b0b4dead27cbaca9f
Tetyana <gromova1tanya@gmail.com>
2021-09-14 14:28:26 +0300

    add module2 DateApplication

---
+++ module_2/dateApplication/src/main/java/ua/com/alevel/MainDateApp.java
@@ -0,0 +1,8 @@
+package ua.com.alevel;
+
+public class MainDateApp {
+    public static void main(String[] args) {
+        ParseDateController parseDateController = new ParseDateController();
+        parseDateController.run();
+    }
+}
