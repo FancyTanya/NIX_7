ddc0dc71dc9a329cb09685beebd5161536729a87
Tetyana <gromova1tanya@gmail.com>
2021-09-17 21:25:41 +0300

    add shrtestWayApp

---
+++ module_2/find_shortest_way/src/main/java/ua/com/alevel/FindWayMain.java
@@ -0,0 +1,8 @@
+package ua.com.alevel;
+
+public class FindWayMain {
+    public static void main(String[] args) {
+        CityWayController controller = new CityWayController();
+        controller.startApplication();
+    }
+}
