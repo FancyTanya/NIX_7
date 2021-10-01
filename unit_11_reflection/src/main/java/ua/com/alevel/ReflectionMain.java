c18436437ac96a5b599fb0c85a611c2001c44744
Tatyana <zegers05>
2021-10-01 12:15:13 +0300

    add unit_11

--- unit_11_reflection/src/main/java/ua/com/alevel/ReflectionMain.java
+++ unit_11_reflection/src/main/java/ua/com/alevel/ReflectionMain.java
@@ -1,6 +1,9 @@
 package ua.com.alevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ua.com.alevel.service.FileService;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.security.KeyStore;
 import java.util.Map;
@@ -9,22 +12,20 @@ import java.util.Set;
 
 public class ReflectionMain {
 
+    private final static Logger logger = LoggerFactory.getLogger(FileService.class);
+
     public static void main(String[] args) {
-        Properties properties = new FileService().getFieldsFromFile();
 
-        Set<Map.Entry<Object, Object>> entrySet = properties.entrySet();
-
-        Factory<AppProperties> factory = new Factory<>();
-
-        AppProperties app = new AppProperties();
-        Class appClass = app.getClass();
-        Field[] fields = appClass.getDeclaredFields();
-        for (Field myField: fields) {
-            AppProperties map = factory.map(appClass, properties);
+        FileService properties = new FileService();
+        try {
+            properties.setFields();
+        } catch (ClassNotFoundException e) {
+            logger.warn(e.getMessage());
+        } catch (IOException e) {
+            logger.warn(e.getMessage());
+        } catch (IllegalAccessException e) {
+            logger.warn(e.getMessage());
         }
 
-        for (Object obj: properties.entrySet()) {
-
-        }
     }
 }
--- unit_11_reflection/src/main/java/ua/com/alevel/service/FileService.java
+++ unit_11_reflection/src/main/java/ua/com/alevel/service/FileService.java
@@ -1,37 +1,59 @@
 package ua.com.alevel.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import ua.com.alevel.AppProperties;
 
+import ua.com.alevel.AppProperties;
+import ua.com.alevel.PropertyKey;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Constructor;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class FileService {
 
-    private final static Logger logger = LoggerFactory.getLogger(FileService.class);
-
-    public Properties getFieldsFromFile() {
+    public void setFields() throws ClassNotFoundException, IOException, IllegalAccessException {
         Properties properties = new Properties();
         InputStream input = FileService.class.getResourceAsStream("/appProperties.properties");
-        try {
+        if (input != null) {
             properties.load(input);
-        } catch (IOException e) {
-            logger.warn(e.getMessage());
+        } else {
+            throw new FileNotFoundException("file not founded");
         }
-        return properties;
+        Field[] annotatedFields = findAnnotatedFields(Class.forName("ua.com.alevel.AppProperties"), PropertyKey.class);
+        for (Field field: annotatedFields) {
+            if (field.getType() == String.class) {
+                    field.set(AppProperties.class, properties.getProperty("property.string"));
+            }
+            if (field.getType() == int.class) {
+                field.set(AppProperties.class, properties.getProperty("property.int"));
+            }
+            if (field.getType() == long.class) {
+                field.set(AppProperties.class, properties.getProperty("property.long"));
+            }
+            if (field.getType() == double.class) {
+                field.set(AppProperties.class, properties.getProperty("property.double"));
+            }
+            if (field.getType() == char.class) {
+                field.set(AppProperties.class, properties.getProperty("property.char"));
+            }
+            if (field.getType() == boolean.class) {
+                field.set(AppProperties.class, properties.getProperty("property.boolean"));
+            }
+        }
     }
 
-    public AppProperties setFields() throws ClassNotFoundException
-            , NoSuchMethodException, InvocationTargetException
-            , InstantiationException, IllegalAccessException {
-        Class appPropClass = Class.forName("ua.com.alevel.AppProperties");
-        Constructor<AppProperties> constructor = appPropClass.getConstructor();
-        AppProperties appProperties = constructor.newInstance();
-        appProperties.
+
+    public static Field[] findAnnotatedFields(Class<?> clazz, Class<? extends Annotation> annotationClass) {
+        Field[] declaredFields = clazz.getDeclaredFields();
+        List<Field> annotatedFields = new ArrayList<>(declaredFields.length);
+        for (Field field: declaredFields) {
+            if (field.isAnnotationPresent(annotationClass)) {
+                annotatedFields.add(field);
+            }
+        }
+        return annotatedFields.toArray(new Field[annotatedFields.size()]);
     }
 }
